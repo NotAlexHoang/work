1c0867d9d01bdd8a98ac7937f17c8649ed89a37b
Alex Hoang <alex.hoang.052@k12.friscoisd.org>
2019-12-19 13:58:52 -0600

    committing life? haha jk... unless...

--- roloc/GameScene.swift
+++ roloc/GameScene.swift
@@ -1,74 +1,62 @@
-//
-//  GameScene.swift
-//  roloc
-//
-//  Created by Hoang, Alex on 12/11/19.
-//  Copyright © 2019 hmdwc. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene,SKPhysicsContactDelegate {
     
-    var ball = SKSpriteNode()
-    var paddle = SKSpriteNode()
-    var score = [Int]()
-    
-    func startGame() {
-        score = [0,0]
-    }
-    
-    func addScore(player: SKSpriteNode) {
-        
-    }
+    var counter = 0
+    var hitcount = 0
+    var ball:SKSpriteNode!
+    var paddle:SKSpriteNode!
+    var score = SKLabelNode(fontNamed: "Helvetica Neue UltraLight 6")
     
     override func didMove(to view: SKView) {
-        ball = self.childNode(withName: "Ball") as! SKSpriteNode
-        paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
+        ball = self.childNode(withName: "Ball") as? SKSpriteNode
+        paddle = self.childNode(withName: "Paddle") as? SKSpriteNode
         
-        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        ball.physicsBody?.applyImpulse(CGVector(dx: 35, dy: 35))
         
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
         border.friction = 0
-        border.restitution = 0
-        
         self.physicsBody = border
+        
+        self.physicsWorld.contactDelegate = self
+        score.text = String(counter)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: self)
-            
-            paddle.run(SKAction.moveTo(x: location.x, duration: 0))
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: self)
-            
-            paddle.run(SKAction.moveTo(x: location.x, duration: 0))
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
         }
     }
     
-    func didBegin(contact: SKPhysicsContact) {
-        let bodyA = contact.bodyA.node?.name
-        let bodyB = contact.bodyB.node?.name
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyAName = contact.bodyA.node?.name
+        let bodyBName = contact.bodyB.node?.name
         
-        if bodyA == "Ball" && bodyB == "Brick" || bodyA == "Brick" && bodyB == "Ball" {
-            contact.bodyA.node?.removeFromParent()
-        } else if bodyB == "Brick" {
-            contact.bodyB.node?.removeFromParent()
+        if bodyAName == "Ball" && bodyBName == "Brick" || bodyAName == "Brick" && bodyBName == "Ball"{
+            
+            if bodyAName == "Brick" {
+                contact.bodyA.node?.removeFromParent()
+                counter += 1
+                
+            } else if bodyBName == "Brick" {
+                contact.bodyB.node?.removeFromParent()
+                counter += 1
+            }
         }
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
-        if ball.position.y <= -640
-        {
-            score = [0,0]
+        if ball.position.y <= -640 {
             ball.position.x = paddle.position.x
             ball.position.y = -455
         }
